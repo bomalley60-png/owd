66708abf1afa4ad7bc90fb59ffdca864f831dcef
bomalley60-png <bomalley60@gmail.com>
2025-08-13 13:50:36 -0400

    Add files via upload

---
+++ JSsheet.js
@@ -0,0 +1,39 @@
+  const hamMenu = document.querySelector('.hamburger');
+  const offScreenMenu = document.querySelector('.off_screen_menu');
+  hamMenu.addEventListener('click', () => {
+    offScreenMenu.classList.toggle('active');
+  })
+
+  function myFunction(x) {
+  x.classList.toggle("change");
+} 
+  var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+} 
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
